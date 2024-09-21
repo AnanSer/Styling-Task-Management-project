a71ee85b14374f5e960fcc1d8a82dcafaf720ca3
AnanSer <ananserbesa@gmail.com>
2024-09-21 23:37:14 +0300

    making dynamic courseinput

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,45 @@
 import React, { useState } from "react";
 
+import styled from "styled-components";
+
 import Button from "../../UI/Button/Button";
 import "./CourseInput.css";
+
+const formControl = styled.div`
+   {
+    margin: 0.5rem 0;
+  }
+
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+  }
+
+  &.invalid {
+    /* background-color: rgb(238, 155, 155); */
+    border-color: red;
+  }
+
+  &.invalid label {
+    color: red;
+  }
+
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid #ccc;
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+
+  & input:focus {
+    outline: none;
+    background: #fad0ec;
+    border-color: #8b005d;
+  }
+`;
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
